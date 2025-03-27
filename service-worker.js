360fa4f9aa0b7e62530d5a17b0ff849203b2791c
Samara Brito <88163981+britosamara@users.noreply.github.com>
2025-03-27 08:41:12 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,25 @@
 self.addEventListener('install', event => {
+  console.log('[Service Worker] Instalando...');
+
   event.waitUntil(
     caches.open('calculadora-cache').then(cache => {
+      console.log('[Service Worker] Cacheando arquivos');
       return cache.addAll([
         './',
         './index.html',
         './style.css',
         './script.js',
         './manifest.json',
-        './background.jpg'
+        './background.jpg',
+        './icon-192.png',
+        './icon-512.png'
       ]);
     })
   );
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Ativado!');
 });
 
 self.addEventListener('fetch', event => {
